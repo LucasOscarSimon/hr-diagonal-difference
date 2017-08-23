8a00c6c2e15171ffc28fcf3427b83d2f0b97953d
LucasSimon <lucas.oscar.simon@gmail.com>
2017-08-22 22:21:03 -0300

    Diagonal Difference - Solved

--- DiagonalDifference/Program.cs
+++ DiagonalDifference/Program.cs
@@ -8,8 +8,26 @@ namespace DiagonalDifference
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[][] a = new int[n][];
+            var primaryDiagonal = new int[n];
+            var secondaryDiagonal = new int[n];
+            var count = 0;
+            var negCount = n;
+            for (int a_i = 0; a_i < n; a_i++)
+            {
+                string[] a_temp = Console.ReadLine().Split(' ');
+                a[a_i] = Array.ConvertAll(a_temp, Int32.Parse);
+                primaryDiagonal[a_i] = primaryDiagonal[a_i] + a[a_i][count];
+                secondaryDiagonal[a_i] = secondaryDiagonal[a_i] + a[a_i][negCount - 1];
+                count = count + 1;
+                negCount = negCount - 1;
+            }
+            var primaryDiagonalSumRes = primaryDiagonal.Sum();
+            var secondaryDiagonalSumRes = secondaryDiagonal.Sum();
+            Console.WriteLine("The absolute value is: {0}", Math.Abs(primaryDiagonalSumRes - secondaryDiagonalSumRes));
         }
     }
 }
